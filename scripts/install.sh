f44eb874087a420abd015ef2e605ae57f9b82a08
eirikhanasand <eirik.hanasand@gmail.com>
2025-12-13 21:36:07 +0100

    fixed spacing

--- scripts/install.sh
+++ scripts/install.sh
@@ -412,6 +412,7 @@ EOF
 sysctl --system
 
 # ----- Installs 1Password CLI -----
+
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
 sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg && \
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | \
@@ -423,7 +424,6 @@ sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22 && \
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
 sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg && \
 sudo apt update && sudo apt install 1password-cli
-
 
 # ----- Clones services -----
 
